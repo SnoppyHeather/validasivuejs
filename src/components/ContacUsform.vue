ba8f156b2ef6add218304724cd38921de3d30149
SnoppyHeather <erwin.prayoga@ti.ukdw.ac.id>
2020-07-29 19:00:32 +0700

    first commit

---
+++ src/components/ContacUsform.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="contacus">
+    <div class="field">
+  <label class="label">Name</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Enter Your Name">
+    </div>
+</div>
+
+<div class="field">
+  <label class="label">Email</label>
+    <div class="control has-icons-left has-icons-right">
+      <input class="input" type="email" placeholder="Enter Your Email">
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+    </div>
+</div>
+
+
+<div class="field">
+  <label class="label">Message</label>
+  <div class="control">
+    <textarea class="textarea" placeholder="Enter Your Message"></textarea>
+  </div>
+</div>
+
+<div class="field">
+  <div class="control">
+    <label class="checkbox">
+      <input type="checkbox">
+      I agree to the <a href="#">terms and conditions</a>
+    </label>
+  </div>
+</div>
+
+
+<div class="field is-grouped">
+  <div class="control">
+    <button class="button is-link">Submit</button>
+  </div>
+</div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/views/Contac.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="has-text-contered">
+      <h1>Contac Us</h1>
+      <img  alt="Vue logo" src="../assets/logo.png" >
+      <div class="columns">
+        <div class="column">
+          <div class="">
+              <ContacUsform />
+        </div>
+      </div>
+      
+      <div class="column">
+        <div class="">
+        </div>
+      </div>  
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+<script>
+import '../css/bootstrap.min.css'
+import '../css/bootstrap-reboot.min.css'
+import '../css/style.css'
+import '../css/animate.css'
+import ContacUsform from '@/components/ContacUsform.vue'
+export default {
+  components: {
+    ContacUsform
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+      margin: 40px 0 0;
+    }
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+    a {
+      color: #42b983;
+    }
+</style>
